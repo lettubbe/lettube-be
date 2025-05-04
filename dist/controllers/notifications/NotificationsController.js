4fac86d3b41cc433e66fef782920aa8259645d5d
Isaiah Daniel <isaiahdaniel203@gmail.com>
2025-05-04 09:11:30 +0100

    notifications controller setup

---
+++ dist/controllers/notifications/NotificationsController.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getUserPushToken = void 0;
+// @desc    Add User Push Token
+// @route    GET /api/v1/notifications/pushToken
+// @access   Private
+const getUserPushToken = () => {
+};
+exports.getUserPushToken = getUserPushToken;
