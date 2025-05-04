fdfac1628ec16989d2bcd9249d0510f4f14e0fc1
Isaiah Daniel <isaiahdaniel203@gmail.com>
2025-05-04 09:11:30 +0100

    notifications controller setup

---
+++ src/controllers/notifications/NotificationsController.ts
@@ -0,0 +1,10 @@
+import asyncHandler from "express-async-handler";
+
+// @desc    Add User Push Token
+// @route    GET /api/v1/notifications/pushToken
+// @access   Private
+
+
+export const getUserPushToken = () => {
+    
+}
